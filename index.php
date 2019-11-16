65ff93d3a84941df236d3500f52e3c1f530a0678
Jayan Anuranga <jayan.anuranga@gmail.com>
2019-11-16 23:28:23 +0530

    remove B Branch Name

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ public function index()
  echo "in the index KK LL MM function A branch Change - A modification";
  echo "in the index KK LL MM function A branch B Change";
 
+ echo "in the index KK LL MM function A branch";
+
 }
 }
