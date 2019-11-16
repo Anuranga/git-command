c59ac79e8e349d2f871246a033d9c28538797d9c
Jayan Anuranga <jayan.anuranga@gmail.com>
2019-11-16 22:57:24 +0530

    second commit from branch B

--- index.php
+++ index.php
@@ -3,5 +3,7 @@ class TestGitCommands{
 public function index()
 {
  echo "in the index KK LL MM function A branch Change - A modification";
+ echo "in the index KK LL MM function A branch B Change";
+
 }
 }
