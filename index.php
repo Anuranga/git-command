a5a2137d45b8f997f010fa13b23fb7edaeaafc19
Jayan Anuranga <jayana@lkpc.exetel>
2020-01-01 07:43:41 +0530

    change echo A

--- index.php
+++ index.php
@@ -2,28 +2,6 @@
 class TestGitCommands{
 public function index()
 {
- echo "in the index KK LL MM function A branch Change - A modification";
-
- echo "in the index KK LL MM function A branch B Change";
-
- echo "in the index KK LL MM function A branch";
-
- echo "in the index KK LL MM function A branch -modification";
- 
- echo "B branch modification after rebase";
- 
- echo "New commit message";
-
- echo "A branch new echo line";
- 
- echo "A branch second echo line";
- 
- echo "A branch new echo line";
-
- 
- echo "Branch B change";
- 
-
- echo "B ---------------";
+	echo "Branch A";
 }
 }
