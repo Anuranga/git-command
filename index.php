a643ea649466b4c8f5bfa910cc0295688745b5ab
Jayan Anuranga <jayana@lkpc.exetel>
2020-01-01 13:28:28 +0530

    5th change for A

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php 
 class TestGitCommands{
 public function index()
+<<<<<<< HEAD
  {
  echo "Branch B";	
  
  echo "branch B 2nd modification";
 
  }
+=======
+{
+	echo "Branch A";
+	
+	echo "changes to commit";
+	
+	echo " branch A modification";
+	
+	echo " branch A third modification";
+	
+	echo " brach copy of A";
+
+	echo "A branch chnage after meger and pull master with branch A";
+
+	echo "5th change for branch A";
+
+}
+>>>>>>> 5th change for A
 }
