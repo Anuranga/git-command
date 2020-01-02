e53e12e85a7b4ac8d7194a3f8d47d62b3216baa9
Jayan Anuranga <jayana@lkpc.exetel>
2020-01-02 09:52:01 +0530

    git pull origin master and set modification

--- index.php
+++ index.php
@@ -1,39 +1,12 @@
 <?php 
 class TestGitCommands{
 public function index()
-<<<<<<< HEAD
-<<<<<<< HEAD
- {
- echo "Branch B";	
- 
- echo "branch B 2nd modification";
-
- }
-=======
-{
-	echo "Branch A";
-	
-	echo "changes to commit";
-	
-	echo " branch A modification";
-	
-	echo " branch A third modification";
-	
-	echo " brach copy of A";
-
-	echo "A branch chnage after meger and pull master with branch A";
-
-	echo "5th change for branch A";
-
-	
-	echo "6th change for branch A";
-	
-	echo "After Rename";
-=======
 {
 
  echo "A Rename";	
->>>>>>> branch Commit
+
+ echo "after git pull origin master";
+
 }
->>>>>>> 5th change for A
+
 }
